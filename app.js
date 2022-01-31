2047c9913d4144b4154936b9be476f0742bde859
Ararat Asatryan <ar.asatryan94@gmail.com>
2022-01-31 21:13:02 +0400

    feat(products): added routes and error handling

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express();
 
-app.use((req, res, next) => {
-    // console.log('Logging HTTP Request Structure>>>>>', req);
-    res.json({
-        message: "Server Started...!"
-    })
+const productRoutes = require('./src/rest-api/routes/products');
+app.use("/products", productRoutes);
+
+app.use( (req, res, next) => {
+    const error = new Error('Request Not Found...')
+    error.status = 404;
+    next(error); 
 });
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 405)
+    res.send({
+        error: error.message
+    })
+})
+
+// app.use((req, res, next) => {
+//     // console.log('Logging HTTP Request Structure>>>>>', req);
+//     res.json({
+//         message: "Server Started...!"
+//     })
+// });
 
 
 module.exports = app;

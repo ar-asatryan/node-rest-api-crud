7ecd936a3d208181be9c470e4ac9dcbd016e39fc
ar-asatryan <ar.asatryan@gmail.com>
2022-02-08 21:50:41 +0400

    feat(controller): added products controller

--- app.js
+++ app.js
@@ -4,35 +4,28 @@ const app = express();
 const productRoutes = require('./src/rest-api/routes/products');
 const orderRoutes = require('./src/rest-api/routes/orders')
 
-// app.use('/', (req, res, next) => {
-//     res.json({
-//         message: "Just LocalHost Requests......>>>>>>>>>>>>>>>>>"
-//     })
-//     next()
-// })
+// app.use( (req, res) => res.json({
+//     message: "Server Started..."
+// }) )
 
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes)
 
 app.use( (req, res, next) => {
     const error = new Error('Request Not Found...')
-    error.status = 404;
+    error.status = 604;
     next(error); 
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 405)
+    res.status(error.status || 605)
     res.send({
         error: error.message
     })
 })
 
-// app.use((req, res, next) => {
-//     // console.log('Logging HTTP Request Structure>>>>>', req);
-//     res.json({
-//         message: "Server Started...!"
-//     })
-// });
-
-
-module.exports = app;
+module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require("http");
 const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config()
+const dotenv = require('dotenv').config();
 
 const port = process.env.PORT;
+
+// const mongoPath = process.env.MONGO_PATH;
+// console.log("mongoPath", mongoPath);
 
 const server = http.createServer(app);
 
